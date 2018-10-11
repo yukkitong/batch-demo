275bd2301c96a643528ae14c7372a42dcb34b115
jason <jason98.bae@gmail.com>
2018-10-12 08:56:27 +0900

    update

--- src/main/java/com/jason/example/batchdemo/BatchDemoApplication.java
+++ src/main/java/com/jason/example/batchdemo/BatchDemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BatchDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BatchDemoApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(BatchDemoApplication.class, args);
+  }
 }
